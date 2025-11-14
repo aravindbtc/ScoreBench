a09bc022f336e3dd46d06a79ae5101fad5fc882f
Aravind S <sivam3882@gmail.com>
2025-11-14 14:24:25 +0000

    okay now what about logo img logo only is visible for me rhat looks very

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/icon.svg" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
