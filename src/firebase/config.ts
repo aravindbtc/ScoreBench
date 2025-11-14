7c43ab19e85c9e58b769e3fdb3adbfcd481ce85d
Aravind S <sivam3882@gmail.com>
2025-11-14 15:50:36 +0000

    then i need to update in github and local system right so give comands

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "studio-967090519-ed75c",
   "appId": "1:924763012199:web:c704ac67a79647a52b68c2",
   "apiKey": "AIzaSyCJk4gBrMKBjfkuR_Yl9bjBrHUNF15MHDE",
   "authDomain": "studio-967090519-ed75c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "924763012199"
+  "storageBucket": "studio-967090519-ed75c.appspot.com",
+  "messagingSenderId": "924763012199",
+  "measurementId": "G-5G3W933X7W"
 };
